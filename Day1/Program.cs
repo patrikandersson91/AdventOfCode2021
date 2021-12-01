67870b00ddb2d458089afe18c45b88ce39e30389
Patrik Andersson <patrika91@gmail.com>
2021-12-01 23:02:40 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
+
+int count = 0;
+int? previous = null;
+var list = new List<int>();
+
+foreach (string nr in File.ReadLines(filePath))
+{
+    list.Add(Convert.ToInt32(nr));
+
+    if (list.Count == 3)
+    {
+        int depth = list.Sum();
+
+        if (previous is not null && depth > previous)
+        {
+            count++;
+        }
+        previous = depth;
+        list.Remove(list.First());
+    }
+}
+
+Console.WriteLine(count);
