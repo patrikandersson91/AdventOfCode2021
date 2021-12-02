ff4a51c537ca44b4cf8aed50d83cb1c50ab40e79
Patrik Andersson <patrika91@gmail.com>
2021-12-02 06:53:01 +0100

    Added Day2 to solution

---
+++ Day2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Day2;
+
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
+var sub = new Submarine();
+
+foreach (string line in File.ReadLines(filePath))
+{
+    var separator = line.Split(" ");
+    var command = separator[0];
+    var amount = Convert.ToInt32(separator[1]);
+
+    switch (command)
+    {
+        case "forward":
+            sub.Distance += amount;
+            sub.Depth += amount * sub.Aim;
+            break;
+        case "down":
+            sub.Aim += amount;
+            break;
+        case "up":
+            sub.Aim -= amount;
+            break;
+    }
+}
+Console.WriteLine(sub.Depth * sub.Distance);
---
+++ Day2/Submarine.cs
@@ -0,0 +1,9 @@
+﻿namespace Day2
+{
+    public class Submarine
+    {
+        public int Depth { get; set; } = 0;
+        public int Distance { get; set; } = 0;
+        public int Aim { get; set; } = 0;
+    }
+}
