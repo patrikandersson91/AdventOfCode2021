47e258a76ca787e912daeeba9edba2c8e688ebf2
Patrik Andersson <patrika91@gmail.com>
2021-12-06 08:38:15 +0100

    Refactored day6

--- Day6/Tasks.cs
+++ Day6/Tasks.cs
@@ -2,42 +2,47 @@
 {
     public class Tasks
     {
-        public int Task1()
+        public long Task1()
         {
             List<int> fishes = GetList();
+            long[] cycles = InsertCycles(fishes);
 
-            for (int i = 0; i < 80; i++)
-            {
-                List<int> newFishes = new();
+            cycles = GetFishCyclesAfterDays(cycles, 80);
 
-                for (int j = 0; j < fishes.Count; j++)
-                {
-                    if (fishes[j] < 1)
-                    {
-                        fishes[j] = 6;
-                        newFishes.Add(8);
-                    }
-                    else
-                    {
-                        fishes[j]--;
-                    }
-                }
-                fishes.AddRange(newFishes);
-            }
-            return fishes.Count();
+            return cycles.Sum();
         }
 
         public long Task2()
         {
             List<int> fishes = GetList();
+            long[] cycles = InsertCycles(fishes);
 
-            long[] cycle = new long[9];
+            cycles = GetFishCyclesAfterDays(cycles, 256);
+
+            return cycles.Sum();
+        }
+
+        private List<int> GetList()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
+            return new(File.ReadAllText(filePath).Split(",").Select(int.Parse).ToList());
+        }
+
+        private long[] InsertCycles(List<int> fishes)
+        {
+            long[] cycles = new long[9];
+
             for (int i = 0; i <= 8; i++)
             {
-                cycle[i] = fishes.Count(x => x == i);
+                cycles[i] = fishes.Count(x => x == i);
             }
 
-            for (int i = 0; i < 256; i++)
+            return cycles;
+        }
+
+        private long[] GetFishCyclesAfterDays(long[] cycle, int days)
+        {
+            for (int i = 0; i < days; i++)
             {
                 long newFishes = 0;
                 long[] newCycle = new long[9];
@@ -50,20 +55,13 @@
                     }
                     else
                     {
-                        newCycle[j-1] += cycle[j];
+                        newCycle[j - 1] += cycle[j];
                     }
                 }
                 newCycle[8] = newFishes;
                 cycle = newCycle;
             }
-
-            return cycle.Sum();
-        }
-
-        private List<int> GetList()
-        {
-            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
-            return new(File.ReadAllText(filePath).Split(",").Select(int.Parse).ToList());
+            return cycle;
         }
     }
 }
