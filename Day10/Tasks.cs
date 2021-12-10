3ee4bdb0e45b9812525ae8d984089b0033f60626
Patrik Andersson <patrika91@gmail.com>
2021-12-10 17:23:38 +0100

    Minor refactor on day10

--- Day10/Tasks.cs
+++ Day10/Tasks.cs
@@ -9,20 +9,7 @@ namespace Day10
         public int Task1()
         {
             var raw = GetList();
-            List<char> illegal = new();
-            foreach (var line in GetList())
-            {
-                string tempCharacters = "";
-                foreach (var c in line)
-                {
-                    if (pairs.Keys.Contains(c)) { tempCharacters += c; }
-                    else if (pairs.TryGetValue(tempCharacters.Last(), out char x) && x == c)
-                    {
-                        tempCharacters = tempCharacters.Remove(tempCharacters.Length - 1);
-                    }
-                    else { illegal.Add(c); break; }
-                }
-            }
+            List<char> illegal = GetIllegal(GetList()).Item1;
             var points = new Dictionary<char, int> { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 } };
             return illegal.Sum(x => points[x]); // 367227
         }
@@ -30,11 +17,39 @@ namespace Day10
         public long Task2()
         {
             var raw = GetList();
-            List<string> incompleted = new(raw);
-            foreach (var line in raw)
+            List<string> incompleted = raw.Where(x=> !GetIllegal(raw).Item2.Contains(x)).ToList();
+            List<List<char>> complementaryList = GetComplementaryCharactersList(incompleted);
+
+            var points = new Dictionary<char, int> { { ')', 1 }, { ']', 2 }, { '}', 3 }, { '>', 4 } };
+            var results = complementaryList.Select(x => x.Aggregate(0L, (x, y) => x * 5 + points[y])).ToList();
+            return results.OrderBy(x => x).Skip((complementaryList.Count - 1) / 2).First(); // 3583341858 
+        }
+
+        private List<List<char>> GetComplementaryCharactersList(List<string> list) {
+            List<List<char>> complementaryList = new();
+            foreach (var row in list)
             {
                 string tempCharacters = "";
-                foreach (var c in line)
+                foreach (var c in row)
+                {
+                    if (pairs.Keys.Contains(c)) { tempCharacters += c; }
+                    else { tempCharacters = tempCharacters.Remove(tempCharacters.Length - 1); }
+                }
+                if (tempCharacters.Length > 0)
+                {
+                    complementaryList.Add(tempCharacters.Reverse().Select(x => pairs[x]).ToList());
+                }
+            }
+            return complementaryList;
+        }
+        private (List<char>, List<string>) GetIllegal(List<string> list)
+        {
+            List<char> illegalChars = new();
+            List<string> illegalList = new();
+            foreach (var row in list)
+            {
+                string tempCharacters = "";
+                foreach (var c in row)
                 {
                     if (pairs.Keys.Contains(c)) { tempCharacters += c; }
                     else if (pairs.TryGetValue(tempCharacters.Last(), out char x) && x == c)
@@ -43,34 +58,14 @@ namespace Day10
                     }
                     else
                     {
-                        incompleted.Remove(incompleted.First(x => x == line));
+                        illegalChars.Add(c);
+                        illegalList.Add(row);
                         break;
                     }
                 }
             }
-
-            List<List<char>> addedRows = new();
-            foreach (var line in incompleted)
-            {
-                string tempCharacters = "";
-                foreach (var c in line)
-                {
-                    if (pairs.Keys.Contains(c)) { tempCharacters += c; }
-                    else { tempCharacters = tempCharacters.Remove(tempCharacters.Length - 1); }
-                }
-
-                if (tempCharacters.Length > 0)
-                {
-                    List<char> addedChars = tempCharacters.Reverse().Select(x => pairs[x]).ToList();
-                    addedRows.Add(tempCharacters.Reverse().Select(x => pairs[x]).ToList());
-                }
-            }
-
-            var points = new Dictionary<char, int> { { ')', 1 }, { ']', 2 }, { '}', 3 }, { '>', 4 } };
-            var results = addedRows.Select(x => x.Aggregate(0L, (x, y) => x * 5 + points[y])).ToList(); 
-            return results.OrderBy(x => x).Skip((addedRows.Count - 1) / 2).First(); // 3583341858 
+            return (illegalChars, illegalList);
         }
-
         private List<string> GetList()
         {
             string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
