6706b9c193d9f7d6535a759bb17eeeb0fe50e76a
Patrik Andersson <patrika91@gmail.com>
2021-12-13 06:01:55 +0100

    prepared day13

--- Day12/Tasks.cs
+++ Day12/Tasks.cs
@@ -2,83 +2,70 @@
 {
     public class Tasks
     {
-        private List<Connection> rawPaths => GetRawPaths();
+        private List<(string from, string to)> rawPaths => GetRawPaths();
 
         public int Task1()
         {
-            List<List<Connection>> routes = new();
-            List<Connection> route = new();
+            List<List<string>> totalRoutes = new();
+            List<string> route = new();
 
-            var path = rawPaths.First(x => x.Start);
-            PathFind(routes, path, route);
+            PathFind(totalRoutes, route, "start");
 
-            return routes.Count(); // 3802
+            return totalRoutes.Count; // 3802
         }
 
         public int Task2()
         {
-            List<List<Connection>> routes = new();
-            List<Connection> route = new();
+            List<List<string>> totalRoutes = new();
+            List<string> route = new();
+            string? visitedTwice = null;
 
-            var path = rawPaths.First(x => x.Start);
-            Connection? visitedTwice = null;
-            PathFind(routes, path, route, true, visitedTwice);
+            PathFind(totalRoutes, route, "start", true, visitedTwice);
 
-            return routes.Count(); // 99448
+            return totalRoutes.Count; // 99448
         }
 
-        private void PathFind(List<List<Connection>> routes, Connection path, List<Connection> route, bool checkVisitedTwice = false, Connection? visitedTwice = null)
+        private void PathFind(List<List<string>> totalRoutes, List<string> route, string path, bool checkTwice = false, string? visitedTwice = null)
         {
-            if (path.End)
+            if (path == "end")
             {
                 route.Add(path);
-                routes.Add(new(route));
+                totalRoutes.Add(route);
+                Console.WriteLine(string.Join(",", route));
                 route.Remove(route.Last());
                 return;
             }
-            if (path.Small && route.Any(x => x.Node == path.Node))
+
+            if (path.Any(x => char.IsLower(x)) && route.Any(x => x == path))
             {
-                if (!checkVisitedTwice || visitedTwice != null) { return; }
+                if (!checkTwice || visitedTwice != null) { return; }
                 else { visitedTwice = path; }
             }
 
             route.Add(path);
 
-            var connections = rawPaths.Where(x => x.ConnectedTo == path.Node && !x.Start &&
-            !(x.Small && route.Any(r => r.Node == x.Node) && 
-            (!checkVisitedTwice || visitedTwice != null))).ToList();
+            var connections = rawPaths.Where(x => 
+                x.from == path && x.to != "start" &&  
+                !(x.to.Any(x => char.IsLower(x)) && route.Any(r => r == x.to) && (!checkTwice || visitedTwice != null)))
+                .Select(x => x.to).ToList();
+
             foreach (var newPath in connections)
             {
-                PathFind(routes, newPath, route, checkVisitedTwice, visitedTwice);
+                PathFind(totalRoutes, route, newPath, checkTwice, visitedTwice);
             }
             route.Remove(route.Last());
             return;
         }
-        private List<Connection> GetRawPaths()
+        private List<(string from, string to)> GetRawPaths()
         {
             string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
             List<string> rows = new(File.ReadAllLines(filePath));
-            List<Connection> connections = new();
+            List<(string from, string to)> connections = new();
             foreach (var row in rows)
             {
                 var path = row.Split("-");
-                connections.Add(new Connection
-                {
-                    Node = path[0],
-                    ConnectedTo = path[1],
-                    Small = path[0].Any(char.IsLower),
-                    Start = path[0] == "start",
-                    End = path[0] == "end"
-                });
-
-                connections.Add(new Connection
-                {
-                    Node = path[1],
-                    ConnectedTo = path[0],
-                    Small = path[1].Any(x => char.IsLower(x)),
-                    Start = path[1] == "start",
-                    End = path[1] == "end"
-                });
+                connections.Add(new(path[0], path[1]));
+                connections.Add(new(path[1], path[0]));
             }
             return connections;
         }
---
+++ Day13/Tasks.cs
@@ -0,0 +1,23 @@
+﻿namespace Day13
+{
+    public class Tasks
+    {
+        private List<string> rawList => GetList();
+
+        public int Task1()
+        {
+            return 0;
+        }
+
+        public int Task2()
+        {
+            return 0;
+        }
+
+        private List<string> GetList()
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\input.txt");
+            return new(File.ReadAllLines(filePath));
+        }
+    }
+}
