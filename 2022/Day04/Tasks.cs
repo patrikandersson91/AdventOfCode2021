bcf4d00dfdfc1c05b50eb0ffe032cbab38305a0b
Patrik Andersson <patrik.andersson@consid.se>
2022-12-04 06:53:49 +0100

    Fixed Day04 (minor) and prepared Day05.

--- 2022/Day04/Tasks.cs
+++ 2022/Day04/Tasks.cs
@@ -10,19 +10,8 @@ internal class Tasks
 
         foreach (var item in RawList)
         {
-            var elf1From = int.Parse(item.Split(",")[0].Split("-")[0]);
-            var elf1To = int.Parse(item.Split(",")[0].Split("-")[1]);
-
-            var elf2From = int.Parse(item.Split(",")[1].Split("-")[0]);
-            var elf2To = int.Parse(item.Split(",")[1].Split("-")[1]);
-
-            var range1 = Enumerable.Range(elf1From, elf1To - elf1From + 1).ToList();
-            var range2 = Enumerable.Range(elf2From, elf2To - elf2From + 1).ToList();
-
-            if (!range1.Except(range2).Any() || !range2.Except(range1).Any())
-            {
-                pairs++; continue;
-            }
+            var (range1, range2) = GetRange(item);
+            if (!range1.Except(range2).Any() || !range2.Except(range1).Any()) { pairs++; }
         }
 
         return pairs;
@@ -34,22 +23,25 @@ internal class Tasks
 
         foreach (var item in RawList)
         {
-            var elf1From = int.Parse(item.Split(",")[0].Split("-")[0]);
-            var elf1To = int.Parse(item.Split(",")[0].Split("-")[1]);
-
-            var elf2From = int.Parse(item.Split(",")[1].Split("-")[0]);
-            var elf2To = int.Parse(item.Split(",")[1].Split("-")[1]);
-
-            var range1 = Enumerable.Range(elf1From, elf1To - elf1From + 1).ToList();
-            var range2 = Enumerable.Range(elf2From, elf2To - elf2From + 1).ToList();
-
-            if (range1.Any(range2.Contains) || range2.Any(range1.Contains))
-            {
-                pairs++; continue;
-            }
+            var (range1, range2) = GetRange(item);
+            if (range1.Any(range2.Contains) || range2.Any(range1.Contains)) { pairs++; }
         }
 
         return pairs;
+    }
+
+    private (List<int> range1, List<int> range2) GetRange(string item)
+    {
+        var x1 = int.Parse(item.Split(",")[0].Split("-")[0]);
+        var x2 = int.Parse(item.Split(",")[0].Split("-")[1]);
+
+        var y1 = int.Parse(item.Split(",")[1].Split("-")[0]);
+        var y2 = int.Parse(item.Split(",")[1].Split("-")[1]);
+
+        var range1 = Enumerable.Range(x1, x2 - x1 + 1).ToList();
+        var range2 = Enumerable.Range(y1, y2 - y1 + 1).ToList();
+
+        return (range1, range2);
     }
 
     private List<string> GetRawList()
