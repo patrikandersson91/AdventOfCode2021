b6c668b1ffa8862fea05205b21376dcfc5ecf9e1
Patrik Andersson <patrika91@gmail.com>
2023-12-01 16:39:07 +0100

    Prepared Day02.

---
+++ 2023/Day02/Tasks.cs
@@ -0,0 +1,22 @@
+﻿namespace Day02;
+
+internal class Tasks
+{
+    private List<string> RawList => GetRawList();
+
+    internal int Task1()
+    {
+        return -1;
+    }
+
+    internal int Task2()
+    {
+        return -1;
+    }
+
+    private List<string> GetRawList()
+    {
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
+        return File.ReadAllText(filePath).Split("\r\n").ToList();
+    }
+}
